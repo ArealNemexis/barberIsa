f8cbb7f64e720bccbfac60b88c32143995020aec
Isabella Ricci <ricciisa05@gmail.com>
2020-09-08 20:54:33 -0300

    remoção de comentario em index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,36 +11,3 @@ app.use(router)
 app.listen(port)
 
 
-/*
-app.get("/", (request, response)=>{
-    return response.json({mensagem:"bella"})
-})
-
-app.post("/usuario", (request, response)=>{
-    var { nome, sobrenome, email, senha } = request.body
-    var usuario = {"nome":nome, "sobrenome":sobrenome, "email":email, "senha":senha }
-    vetor.push(usuario)
-    
-    fs.appendFile('bd.txt', `${usuario.nome},${usuario.sobrenome},${usuario.email},${usuario.senha}\n`)
-    
-
-    return response.json({message:"sucesso"})
-})
-
-app.get("/usuario", (request, response)=>{
-    response.json(vetor)
-})
-
-app.get("/usuario/:id?", (request, response)=>{
-    var { id } = request.params
-    console.log(id);
-    response.json(vetor[id])
-})
-
-app.get("/usuarioquery", (request, response)=>{
-    var {dados1, dados2} = request.query
-
-    console.log(dados1)
-    response.json({dado:vetor[0][dados1]})
-})
-*/
